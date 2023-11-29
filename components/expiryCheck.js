cc8f271fc0af9a6f5e744918464f634fa9f33c70
sahil <sahil2742002@gmail.com>
2023-11-29 09:55:21 +0530

    expiry check router push added

--- components/expiryCheck.js
+++ components/expiryCheck.js
@@ -1,14 +1,21 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import { useRouter } from 'next/navigation';
-export default function expiryCheck(){
-const router=useRouter();
-    const expirationTime = jwt.verify(JSON.parse(localStorage.getItem('myUser')).token,"Iam@User")
+
+export default function expiryCheck() {
+  const router = useRouter();
+  const storedUser = JSON.parse(localStorage.getItem('myUser'));
+
+  if (storedUser && storedUser.token) {
+    const expirationTime = jwt.verify(storedUser.token, "Iam@User");
+
     if (expirationTime.exp) {
-      const currentTime = new Date().getTime() /1000;
-      if (currentTime > parseInt(expirationTime.exp)) {
-        // Token has expired, remove it from local storage
-        router.push('/login')
+      const currentTime = new Date().getTime() / 1000;
+
+      if (currentTime > expirationTime.exp) {
+        // Token has expired, remove it from local storage and redirect to login page
         localStorage.removeItem('myUser');
+        router.push('/login');
       }
-    }      
+    }
+  }
 }
